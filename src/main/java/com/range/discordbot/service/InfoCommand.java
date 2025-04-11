06c6e316ff1ff308a73cae874dd30d78dd2fa53d
range79 <darkrange006@gmail.com>
2025-04-11 22:15:48 +0400

    very very very small fix

--- src/main/java/com/range/discordbot/service/InfoCommand.java
+++ src/main/java/com/range/discordbot/service/InfoCommand.java
@@ -12,7 +12,7 @@ public class InfoCommand {
         event.reply("✨ Sprida is a bot built with Spring and JDA (Java Discord API) designed to bring a variety of helpful features to your Discord server. " +
                 "🚀 Using Spring for backend development and JDA for smooth Discord integration, Sprida allows seamless interactions that enhance the server experience. " +
                 "🤖 We welcome you to explore the project on GitHub and contribute to its growth. " +
-                "Feel free to check it out and be part of the journey here: https://github.com/range79 🌟 " +
+                "Feel free to check it out and be part of the journey here: https://github.com/range79 🌟 "
 //                "Current Version: " + version
 
         ).queue();
